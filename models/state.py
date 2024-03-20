e747115da57703622a1e25e04afd9ff4d5fb342c
marky376 <mmutuamark@gmail.com>
2024-03-20 09:03:01 +0300

    A commit on files for the Airbnb clone

--- models/state.py
+++ models/state.py
@@ -1,8 +1,26 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
-
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from os import getenv
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    state = relationship('City', cascade='all, delete') backref="state")
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship("City", cascade="all, delete", backref="state")
+    else:
+    @property
+    def cities(self):
+        """Getter attribute for cities in FileStorage"""
+        from models import storage
+        cities_list = []
+        for city in storage.all("City").values():
+            if city.state_id == self.id:
+                cities_list.append(city)
+        return cities_list
+
