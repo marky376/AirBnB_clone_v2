0f4f4cd33ada5295770ab901193d0dc9e25fd1a5
marky376 <mmutuamark@gmail.com>
2024-03-20 09:01:55 +0300

    A commit on files for the Airbnb clone

--- models/city.py
+++ models/city.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import ForeignKey, Column, String
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    __tablename__ = 'cities'
+    name = Column(String(128), nullable=False
+    state_id = Column(String(60), nullable=False, ForeignKey('states_id')
+
+    state = relationship("State", back_populates="cities")
