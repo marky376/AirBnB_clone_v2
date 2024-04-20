3d9ea7d7b94af2f3fd9b63a0d7bd6680c61ffba5
marky376 <mmutuamark@gmail.com>
2024-04-20 12:35:49 +0300

    A commit on some file

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,8 +1,19 @@
+#!/usr/bin/python3
+"""
+This script starts a Flask web application listening on 0.0.0.0, port 5000.
+It defines a route that displays "Hello HBNB!" when accessed.
+"""
+
 from flask import Flask
 
-app = FLASK(__name__)
+app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello HBNB!'
 
+@app.route('/', strict_slashes=False)
+def route():
+    """Route function for the root URL."""
+    return ("Hello HBNB!")
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
