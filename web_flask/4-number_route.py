f9cfc1e16682edf75ea7d3df2190bcd0888b8cff
marky376 <mmutuamark@gmail.com>
2024-04-21 19:04:54 +0300

    A commit on some file

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+starts a Flask web application:
+Your web application must be listening on 0.0.0.0, port 5000
+must use the option strict_slashes=False in your route definition
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def route():
+    """display Hello HBNB"""
+    return ("Hello HBNB!")
+
+
+@app.route('/hbnb', strict_slashes=False)
+def route2():
+    """display HBNB"""
+    return ("HBNB")
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    return "C " + text.replace("_", " ")
+
+
+@app.route('/python/', defaults={'text': "is cool"}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text):
+    return "Python " + text.replace("_", " ")
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    return f"{n} is a number"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
